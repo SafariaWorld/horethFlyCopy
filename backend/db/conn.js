04730b5386c67a1336f4ea13f3d7da9a436ee1f6
briandriscollcode <bdriscoll407@gmail.com>
2021-11-29 12:39:00 -0700

    added mongoDB cluster string to var: connectionString v4 fix atlas url again

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const connectionString = process.env.MONGODB_URI || "mongodb+srv://brian:Lfifteenf123@horethflydatabase1.8xeap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connectionString = process.env.MONGODB_URI || "mongodb+srv://brian2:test123@horethflydatabase1.8xeap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
